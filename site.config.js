ecd468d1c1388d92544c6eb629d5a9fa82d629fa
somehow <snsdkf6@gmail.com>
2023-04-28 17:12:21 +0900

    [EDIT] changed url

--- site.config.js
+++ site.config.js
@@ -13,7 +13,7 @@ const CONFIG = {
   projects: [
     {
       name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      href: "https://github.com/gouthiki90/morethan-log",
     },
   ],
   // blog setting (required)
